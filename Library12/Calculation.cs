60f19503c303ba827e226e0cf34437a90bd552e7
KAT KAT <kass.kiss@yandex.ru>
2021-10-22 01:39:42 +0300

    Добавьте файлы проекта.

---
+++ Library12/Calculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library12
+{
+    public class Calculation
+    {
+        static Random random = new Random();
+
+        /// <summary>
+        /// Находит сумму n целых, четных, случайных чисел, распределенных в 
+        /// диапазоне от 0 до count.
+        /// </summary>
+        /// <param name="count">Количество чисел и граница генарции</param>
+        /// <returns>Сумма четных чисел и сгенерированные числа</returns>
+        public static (int sum, string numbers) SumEvenWithTuple(int count)
+        {
+            int sum = 0;
+            string numbers = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = random.Next(0, count);
+                numbers += number.ToString() + " ";
+                sum += number;
+            }
+
+            return (sum, numbers);
+        }
+    }
+}
---
+++ Practice 1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Library12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Гаврюшин К. А. ИСП-34", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int integer = Convert.ToInt32(inputValue.Text);
+            var result = Calculation.SumEvenWithTuple(integer);
+            outputSum.Text = result.sum.ToString();
+            outputFull.Text = result.numbers;
+    }   }
+}
